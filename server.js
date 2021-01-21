aaf84dd5552a88a3b2038108452812dfcd1f00e6
Jared Long <jlong4223@gmail.com>
2021-01-21 15:21:02 -0600

    running server

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("<h1>Home Page</h1>");
 });
+
+const port = process.env.PORT || 3001;
+app.listen(port, function () {
+  console.log(`Server is running on port ${port}`);
+});
