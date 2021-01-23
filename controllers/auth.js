8742f23e4b59185eb9bbc29f5c517afae1334aba
Jared Long <jlong4223@gmail.com>
2021-01-23 16:15:35 -0600

    uses insert query to put into database

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+// stopped at 23min
+
 // import modules in here
 const mysql = require("mysql");
 const jwt = require("jsonwebtoken");
@@ -44,6 +46,25 @@ exports.register = (req, res) => {
 
       let hashedPassword = await bcrypt.hash(password, 8);
       console.log(hashedPassword);
+
+      db.query(
+        "INSERT INTO users SET ?",
+        {
+          name: name,
+          email: email,
+          password: hashedPassword,
+        },
+        (error, results) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log(results);
+            res.render("register", {
+              message: "User registered",
+            });
+          }
+        }
+      );
     }
   );
 };
