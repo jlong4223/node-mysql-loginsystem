57d38be9c8c457c3a22be1522deee34b85e0514f
Jared Long <jlong4223@gmail.com>
2021-01-24 16:28:55 -0600

    passes user data to the index route

--- routes/pages.js
+++ routes/pages.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
 
-router.get("/", (req, res) => {
-  res.render("index");
+router.get("/", authController.isLoggedIn, (req, res) => {
+  res.render("index", {
+    user: req.user,
+  });
 });
 
 router.get("/register", (req, res) => {
